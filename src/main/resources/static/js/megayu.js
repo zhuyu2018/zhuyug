7a4fef5574a74d3d144241edc1d2aa3a0fa87bff
zhuyu2018 <406663924@qq.com>
2017-10-14 23:01:36 +0800

    tj

--- src/main/resources/static/js/megayu.js
+++ src/main/resources/static/js/megayu.js
@@ -1,9 +1,4 @@
-function  mouseThing(obj) {
-    $("#loginOut").find("img").attr("src","/image/lgo2.png");
-}
-function  mouseOutThing() {
-    $("#loginOut").find("img").attr("src","/image/lgo2.png");
-}
+
 function  loginOut(obj) {
     $("#loginOut").find("img").attr("src","/image/lgo2.png");
     $.post("/login/loginOut",{loginout:"out"},function (result) {
